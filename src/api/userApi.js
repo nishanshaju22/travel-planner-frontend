42cf01401ad96399d1b5270fd687565e735b559a
nishanshaju22 <nishan.shaju@icloud.com>
2026-02-05 16:23:51 +0800

    linting resolved

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -6,13 +6,13 @@ export const userApi = {
 		return response.data
 	},
 
-    addToBucketList: async(userId, place) => {
-        const response = await api.post('/user/addBucketList', { userId, place })
-        return response.data
-    },
+	addToBucketList: async(userId, place) => {
+		const response = await api.post('/user/addBucketList', { userId, place })
+		return response.data
+	},
 
-    deleteFromBucketList: async(userId, place) => {
-        const response = await api.delete(`/user/${userId}/showBucketList/${place}`, { userId, place })
-        return response.data
-    },
+	deleteFromBucketList: async(userId, place) => {
+		const response = await api.delete(`/user/${userId}/showBucketList/${place}`, { userId, place })
+		return response.data
+	},
 }
